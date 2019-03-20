c720e5bf75b19bc97b1ce6f0e4ed42c7d30841d4
Guru Team <xguruteam@paulzhang.guru>
2019-03-20 06:12:21 -0400

    Basic iBeacon Scanning Module, only for Estimate iBeacon for now

---
+++ Rovi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rovi
+//
+//  Created by Luccas Beck on 3/19/19.
+//  Copyright © 2019 Luccas Beck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
